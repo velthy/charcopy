d16f610c472034f5df6c0e004f4dca6b47acc6be
Stefan Velthuys <velthy@required.ch>
2018-04-07 10:51:35 +0200

    initial commit with all fies

---
+++ sass/config.rb
@@ -0,0 +1,14 @@
+# gem: sass-css-importer to inline css files
+require 'sass-css-importer'
+
+http_path = "../"
+css_dir = "../css"
+sass_dir = "../sass"
+images_dir = "../img"
+javascripts_dir = "../js"
+relative_assets = true
+
+output_style = :compact
+environment = :production
+
+cache_path = '../sass/_cache'
