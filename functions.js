ea9b7b35274efe71ddd0f4fdfcf37a9d7deefc33
Stefan Velthuys <velthy@required.ch>
2023-03-31 14:52:32 +0200

    vanilla css + color switcher

--- functions.js
+++ functions.js
@@ -18,40 +18,72 @@
 			$( '.reset-search ').removeClass( 'hidden' );
 		}
 	});
+
 	$(document).on('click', '.reset-search', function(e){
 		$( 'input[type="search"]' ).val('').focus();
 		$('.char-cards .char').show();
 		$( this ).addClass( 'hidden' );
 		e.preventDefault();
 	});
+
 	$(document).keyup(function(e) {
 		if ( e.keyCode === 83 || e.keyCode === 70 ) { // s(earch), f(ind), or esc
 			if ( ! $( 'input[type="search"]' ).is( ':focus' ) ) {
 				$( 'input[type="search"]' ).val('').focus();
-				$( 'body' ).removeClass( 'about-open' );
 			}
 		}
 		if ( e.keyCode === 27 ) { esc
 			$( 'input[type="search"]' ).val('').focus();
-			$( 'body' ).removeClass( 'about-open' );
-		}
-	});
-	$(document).keyup(function(e) {
-		if ( e.keyCode === 65 ) { // s(earch), f(ind), or esc
-			if ( ! $( 'input[type="search"]' ).is( ':focus' ) ) {
-				$( 'body' ).addClass( 'about-open' );
-			}
 		}
 	});
 
-	$(document).on('click', '.open-about', function(e){
-		$( 'body' ).addClass( 'about-open' );
+	$(document).on('click', '.color-scheme-selector button', function(e){
+		var color_scheme = $( this ).attr( 'data-color-scheme');
+		$( 'body' ).removeClass( 'color-scheme-yellow color-scheme-green color-scheme-blue color-scheme-purple color-scheme-pink' ).addClass( color_scheme );
+		$( '.color-scheme-selector button' ).removeClass( 'current' );
+		$( this ).addClass( 'current' );
 		e.preventDefault();
 	});
-	$(document).on('click', '.close-about', function(e){
-		$( 'body' ).removeClass( 'about-open' );
-		$( 'input[type="search"]' ).focus();
-		e.preventDefault();
-	});
-
 }(jQuery));
+
+
+/**
+ * Darkmode.
+ */
+
+( () => {
+	const themeToggle = document.getElementById( 'dark-mode-toggle' );
+	if ( ! themeToggle ) {
+		return;
+	}
+
+	let activeTheme = false;
+	if ( document.cookie.split( ';' ).some( ( item ) => item.trim().startsWith( 'charcopy-theme=' ) ) ) {
+		activeTheme = document.cookie.split( '; ' ).find( row => row.startsWith( 'charcopy-theme=' ) ).split( '=' )[1];
+	}
+
+	const setTheme = ( isDark ) => {
+		document.cookie = `charcopy-theme=${ isDark ? 'dark' : 'light' }; path=/`;
+		themeToggle.checked = isDark ? true : false;
+		if ( isDark ) {
+			document.body.classList.add( 'dark-mode');
+			document.body.classList.remove( 'light-mode');
+		} else {
+			document.body.classList.add( 'light-mode');
+			document.body.classList.remove( 'dark-mode');
+		}
+	};
+
+	// When no theme was set, get current system default.
+	if ( false === activeTheme ) {
+		activeTheme = window.matchMedia( '(prefers-color-scheme: dark)' ).matches ? 'dark' : 'light';
+	}
+
+	// Set theme based on localStorage or default system setting.
+	setTheme( 'dark' === activeTheme );
+
+	// Set theme based on toggle.
+	themeToggle.addEventListener( 'click', ( e ) => {
+		setTheme( e.target.checked );
+	} );
+} )();
